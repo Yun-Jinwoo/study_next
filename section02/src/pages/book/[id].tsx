8d2e45c96765207aec2d8c07d94e314ad1748756
Yun-Jinwoo <yjw3480@naver.com>
2025-05-30 18:48:12 +0900

    ✨ feat: 도서 상세페이지 텍스트 부분 추가 구현 및 스타일링

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -17,13 +17,20 @@ export default function Page() {
     mockData;
 
   return (
-    <div>
+    <div className={style.continer}>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
         <img src={coverImgUrl} />
       </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+
+      <div className={style.description}>{description}</div>
     </div>
   );
 }
